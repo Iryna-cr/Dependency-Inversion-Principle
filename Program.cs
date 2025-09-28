7b349e83a59366d282324696121da8bebd497b76
Iryna <kytsenkoiryna33@gmail.com>
2025-09-28 19:59:32 +0300

    DIP

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+interface IPaymentService
+{
+    void Pay(double amount);
+}
+
+class PayPalService : IPaymentService
+{
+    public void Pay(double amount) =>
+        Console.WriteLine($"Paid {amount} via PayPal");
+}
+
+class CardService : IPaymentService
+{
+    public void Pay(double amount) =>
+        Console.WriteLine($"Paid {amount} via Credit Card");
+}
+
+class Checkout
+{
+    private readonly IPaymentService _paymentService;
+
+    public Checkout(IPaymentService paymentService)
+    {
+        _paymentService = paymentService;
+    }
+
+    public void Process(double amount)
+    {
+        _paymentService.Pay(amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IPaymentService paypal = new PayPalService();
+        IPaymentService card = new CardService();
+
+        Checkout checkout1 = new Checkout(paypal);
+        checkout1.Process(200);
+
+        Checkout checkout2 = new Checkout(card);
+        checkout2.Process(500);
+    }
+}
+
